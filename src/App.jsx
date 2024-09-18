b3e4bd4e945f585c223871d21337865beb594803
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-18 13:49:33 +0530

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [counter, setcounter] = useState(0)
+  const Add = ()=>{
+    setcounter(counter+1);
+    if (counter>=20){
+      setcounter(counter)
+    }
+  }
+
+  const Minus = ()=>{
+    setcounter(counter-1);
+    if (counter<=0){
+      setcounter(counter);
+    }
+  }
   return (
     <>
     <div>
-      <h1>Hi This is the starting of the counter Project</h1>
+      <h1>Counter Project</h1>
+      <button onClick={Add}>Add {counter}</button>
+      <button onClick={Minus}>Minus {counter}</button>
     </div>
     </>
   )
